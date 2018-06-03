aac49921e326af587e9cf50a395fa061ea64b18a
Derek Chen <derek.g.chen@gmail.com>
2018-06-03 15:56:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function switchOnCharmFromTip(str) {
       return 'Thank you so much.';
       break;
     case 'not as generous':
-      return 'Thank you';
+      return 'Thank you.';
       break;
     default:
       return 'Bye.'
